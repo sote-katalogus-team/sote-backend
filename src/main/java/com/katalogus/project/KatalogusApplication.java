1c7357556d9dee7c0feb722ef9e425b7fe548298
maryn1421 <zolnaimaryn1421@gmail.com>
2021-01-27 13:34:06 +0100

    Initial commit

---
+++ src/main/java/com/katalogus/project/KatalogusApplication.java
@@ -0,0 +1,13 @@
+package com.katalogus.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatalogusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatalogusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/katalogus/project/KatalogusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katalogus.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatalogusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
