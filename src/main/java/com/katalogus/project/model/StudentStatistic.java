6b7ed328d679aa3daef48d1f0b7ab6f8d2c8901d
Eszkis <laszlo.szkis@gmail.com>
2021-02-01 19:55:34 +0100

    add StudentStatistic

---
+++ src/main/java/com/katalogus/project/model/StudentStatistic.java
@@ -0,0 +1,16 @@
+package com.katalogus.project.model;
+
+public class StudentStatistic {
+
+    private String studentName;
+
+    private String neptunCode;
+
+    private double lecturePercentage;
+
+    private double consultationPercentage;
+
+    private double practicePercentage;
+}
+//százalék számító függvény
+//lista készítés StudentStatistic-l
