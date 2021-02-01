6ef1e4912304e01955f9d43f7c10f248e0c39add
Eszkis <laszlo.szkis@gmail.com>
2021-02-01 20:15:28 +0100

    add multiple repositories

---
+++ src/main/java/com/katalogus/project/repository/TurnusRepository.java
@@ -0,0 +1,7 @@
+package com.katalogus.project.repository;
+
+import com.katalogus.project.entity.Turnus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurnusRepository extends JpaRepository<Turnus, Long> {
+}
