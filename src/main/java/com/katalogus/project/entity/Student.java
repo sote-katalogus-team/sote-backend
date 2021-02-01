fe3c3c347aefbc1aaf30c0743ecb46af0f7ec84b
Eszkis <laszlo.szkis@gmail.com>
2021-02-01 10:45:38 +0100

    add student entity

---
+++ src/main/java/com/katalogus/project/entity/Student.java
@@ -0,0 +1,33 @@
+package com.katalogus.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Student {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String neptun_code;
+
+
+}
