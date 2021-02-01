22f46edb58683ee5c9f6005b14de22cf7e46e166
Eszkis <laszlo.szkis@gmail.com>
2021-02-01 13:21:11 +0100

    add Gyakorlat entity

---
+++ src/main/java/com/katalogus/project/entity/Gyakorlat.java
@@ -0,0 +1,28 @@
+package com.katalogus.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Gyakorlat {
+
+    @Id
+    private Long id;
+
+    private Long teacher_id;
+
+    private Long turnus_id;
+
+    private String name;
+
+    private String code;
+}
