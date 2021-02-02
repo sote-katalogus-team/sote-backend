93b9b0a1d188578b9a7443ec00f79afce6fbc8bc
Eszkis <laszlo.szkis@gmail.com>
2021-02-02 11:14:28 +0100

    add function to find classes by turnuID

--- src/main/java/com/katalogus/project/repository/EloadasRepository.java
+++ src/main/java/com/katalogus/project/repository/EloadasRepository.java
@@ -3,5 +3,9 @@ package com.katalogus.project.repository;
 import com.katalogus.project.entity.Eloadas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EloadasRepository extends JpaRepository<Eloadas, Long> {
+
+    List<Eloadas> findAllByTurnus_id(Long turnusId);
 }
