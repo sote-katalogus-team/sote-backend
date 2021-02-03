46d403ef9570ab6e1b757e55d2c777e4f028a073
Eszkis <laszlo.szkis@gmail.com>
2021-02-03 22:58:25 +0100

    add new function (createWarning) to create warning

--- src/main/java/com/katalogus/project/model/StudentStatistic.java
+++ src/main/java/com/katalogus/project/model/StudentStatistic.java
@@ -1,5 +1,6 @@
 package com.katalogus.project.model;
 
+import com.katalogus.project.entity.Turnus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,5 +21,25 @@ public class StudentStatistic {
     private HashMap<String, Integer> percentages = new HashMap<>();
 
     private String warning;
+
+    public void createWarning(Turnus turnus) {
+        if (percentages.get("lecture") < turnus.getLecture()) {
+            warning = warning.concat("Előadás");
+        }
+        if (percentages.get("practice") < turnus.getPractice()) {
+            if (warning.length() == 0) {
+                warning = warning.concat("Gyakorlat");
+            } else {
+                warning = warning.concat(", Gyakorlat");
+            }
+        }
+        if (percentages.get("consultation") < turnus.getConsultation()) {
+            if (warning.length() == 0) {
+                warning = warning.concat("Konzultáció");
+            } else {
+                warning = warning.concat(", Konzultáció");
+            }
+        }
+    }
 
 }
