ab456efaef8cf786ea61ff5c297f383543d26982
IszalyZs <iszalyzs@gmail.com>
2021-02-03 15:29:47 +0100

    Add student controller

---
+++ src/main/java/com/katalogus/project/controller/StudentController.java
@@ -0,0 +1,48 @@
+package com.katalogus.project.controller;
+
+import com.katalogus.project.entity.Student;
+import com.katalogus.project.service.StudentProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    StudentProvider studentProvider;
+
+    @GetMapping("/all")
+    public List<Student> getAllStudent() {
+        return studentProvider.getAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity saveNewStudent(@RequestBody Student student) {
+        Boolean successful = studentProvider.saveNewStudent(student);
+        if (successful) {
+            return ResponseEntity.ok("New Student created successfully");
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+    @PutMapping("/{student_id}/update")
+    public ResponseEntity updateStudentById(@RequestBody Student student, @PathVariable("student_id") Long studentId) {
+        Boolean successful = studentProvider.updateStudentById(student, studentId);
+        if (successful) {
+            return ResponseEntity.ok("Student updated successfully");
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{student_id}/delete")
+    public void deleteStudentById(@PathVariable("student_id") Long studentId) {
+        studentProvider.deleteStudentById(studentId);
+    }
+}
---
+++ src/main/java/com/katalogus/project/service/StudentProvider.java
@@ -0,0 +1,37 @@
+package com.katalogus.project.service;
+
+import com.katalogus.project.entity.Student;
+import com.katalogus.project.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentProvider {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public List<Student> getAll() {
+        return studentRepository.findAll();
+
+    }
+
+    public Boolean saveNewStudent(Student student) {
+        Object response = studentRepository.save(student);
+        return response.getClass().equals(Student.class);
+
+    }
+
+    public Boolean updateStudentById(Student student, Long studentId) {
+        student.setId(studentId);
+        Object response = studentRepository.save(student);
+        return response.getClass().equals(Student.class);
+    }
+
+    public void deleteStudentById(Long studentId){
+        studentRepository.deleteById(studentId);
+    }
+
+}
