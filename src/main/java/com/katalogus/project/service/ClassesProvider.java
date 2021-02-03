2c3f5ef986ef6e18627d10c431f2ee2c01743f26
Eszkis <laszlo.szkis@gmail.com>
2021-02-03 22:58:58 +0100

    add endpoint("/statistic/{turnus_id}") to get all statistics for a turnus

--- src/main/java/com/katalogus/project/service/ClassesProvider.java
+++ src/main/java/com/katalogus/project/service/ClassesProvider.java
@@ -5,12 +5,13 @@ import com.katalogus.project.entity.Gyakorlat;
 import com.katalogus.project.entity.Konzultacio;
 import com.katalogus.project.entity.Turnus;
 import com.katalogus.project.model.Classes;
-import com.katalogus.project.repository.EloadasRepository;
-import com.katalogus.project.repository.GyakorlatRepository;
-import com.katalogus.project.repository.KonzultacioRepository;
-import com.katalogus.project.repository.TurnusRepository;
+import com.katalogus.project.model.StudentStatistic;
+import com.katalogus.project.repository.*;
+import com.katalogus.project.utility.AttendancePercentage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClassesProvider {
@@ -27,6 +28,12 @@ public class ClassesProvider {
     @Autowired
     KonzultacioRepository konzultacioRepository;
 
+    @Autowired
+    AttendancePercentage attendancePercentage;
+
+    @Autowired
+    StudentRepository studentRepository;
+
 
     public Classes getAllByTurnusId(Long turnusId) {
         Classes classes = new Classes();
@@ -35,5 +42,10 @@ public class ClassesProvider {
         classes.setKonzultacioList(konzultacioRepository.findAllByTurnus_id(turnusId));
 
         return classes;
+    }
+
+    public List<StudentStatistic> getAllStatistic(Long turnusId) {
+        Turnus turnus = turnusRepository.getOne(turnusId);
+        return attendancePercentage.getStudentsStatistics(turnus, studentRepository.findAll(), eloadasRepository.findAll(), gyakorlatRepository.findAll(), konzultacioRepository.findAll());
     }
 }
