e4a6ba5e899ec647e4a73de2c5523f28c0646829
Eszkis <laszlo.szkis@gmail.com>
2021-02-05 11:12:54 +0100

    add enpoint("/{student_id}/send_code") for students. Students now able to attend active classes with correct code

--- src/main/java/com/katalogus/project/service/StudentProvider.java
+++ src/main/java/com/katalogus/project/service/StudentProvider.java
@@ -1,6 +1,6 @@
 package com.katalogus.project.service;
 
-import com.katalogus.project.entity.Student;
+import com.katalogus.project.entity.*;
 import com.katalogus.project.model.Classes;
 import com.katalogus.project.model.StudentStatistic;
 import com.katalogus.project.repository.StudentRepository;
@@ -8,6 +8,7 @@ import com.katalogus.project.utility.AttendancePercentage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -60,5 +61,43 @@ public class StudentProvider {
                     .build();
         }
         return null;
+    }
+
+    public HashMap<Boolean, String> sendInCode(Long studentId, HashMap<String, String> code) {
+        HashMap<Boolean, String> success = new HashMap<>();
+        success.put(false, "Student not found");
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        if (optionalStudent.isPresent()) {
+            Student student = optionalStudent.get();
+            Classes classes = classesProvider.getAllByTurnusId(student.getTurnus_id());
+            List<BasicClass> activeClasses = classes.getActiveClasses();
+            BasicClass currentClass = null;
+            for (BasicClass classToCheck : activeClasses) {
+                if (classToCheck.getCode().equals(code.get("code"))) {
+                    currentClass = classToCheck;
+                }
+            }
+            success.put(false, "Not valid code");
+            if (currentClass != null) {
+                success.remove(false);
+                success.put(true, "Successfully attend at class");
+                if (currentClass.getClass() == Eloadas.class) {
+                    List<Eloadas> eloadasList = student.getEloadasList();
+                    eloadasList.add((Eloadas) currentClass);
+                    student.setEloadasList(eloadasList);
+                } else if (currentClass.getClass() == Gyakorlat.class) {
+                    List<Gyakorlat> gyakorlatList = student.getGyakorlatList();
+                    gyakorlatList.add((Gyakorlat) currentClass);
+                    student.setGyakorlatList(gyakorlatList);
+                } else if (currentClass.getClass() == Konzultacio.class) {
+                    List<Konzultacio> konzultaciList = student.getKonzultacioList();
+                    konzultaciList.add((Konzultacio) currentClass);
+                    student.setKonzultacioList(konzultaciList);
+                }
+                studentRepository.save(student);
+            }
+
+        }
+        return success;
     }
 }
