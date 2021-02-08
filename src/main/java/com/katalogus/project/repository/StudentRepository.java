877df463801ff0670081df1ca106f0bc6963cb76
Eszkis <laszlo.szkis@gmail.com>
2021-02-08 20:36:02 +0100

    add new query("findAllByTurnusId(Long turnusId)") to find students by turnus id

--- src/main/java/com/katalogus/project/repository/StudentRepository.java
+++ src/main/java/com/katalogus/project/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.katalogus.project.repository;
 import com.katalogus.project.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
@@ -10,4 +11,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByNeptunCode(String neptunCode);
 
     Integer countByTurnusId(Long turnusId);
+
+    List<Student> findAllByTurnusId(Long turnusId);
 }
