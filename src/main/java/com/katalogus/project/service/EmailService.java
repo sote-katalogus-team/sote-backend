81388d7002eef3837196b08537b2d6af8ad1bc28
Eszkis <laszlo.szkis@gmail.com>
2021-02-17 10:28:46 +0100

    add from email to env

--- src/main/java/com/katalogus/project/service/EmailService.java
+++ src/main/java/com/katalogus/project/service/EmailService.java
@@ -1,14 +1,14 @@
 package com.katalogus.project.service;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import com.sendgrid.*;
 
 
 @Service
@@ -18,10 +18,12 @@ public class EmailService {
     @Value("${spring.sendgrid.api-key}")
     private String APIKEY;
 
-    private SendGrid sendgrid = new SendGrid(APIKEY);
+    @Value("${spring.mail.username}")
+    private String fromEmail;
 
     public void sendMessage(String email, String code) {
-        Email from = new Email("katalogus@gyerekklinika.com");
+        System.out.println(fromEmail);
+        Email from = new Email(fromEmail);
         String subject = "Validation code";
         Email to = new Email(email);
         Content content = new Content("text/plain", "Dear New User! \n \n Here is your validation code: " + code + "\n \n Sincerely SOTE");
@@ -33,7 +35,10 @@ public class EmailService {
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
-            sg.api(request);
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getHeaders());
+            System.out.println(response.getBody());
         } catch (Exception e) {
             System.out.println("Email: " + email + ". Error: " + e);
         }
